4867ac8db5ab8c1041114cf37065368147a5de51
= <intanmeilanienugraha@gmail.com>
2023-04-04 02:40:26 +0700

    Update

--- src/js/App.test.js
+++ src/js/App.test.js
@@ -1,18 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import Navbar from "../components/Navbar";
-import Me from "../components/Me";
-import AboutMe from "../components/AboutMe";
-import Resume from "../components/Resume";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
+import App from "../App";
 
 test("renders learn react link", () => {
-  render(<Navbar />);
-  render(<Me />);
-  render(<AboutMe />);
-  render(<Resume />);
-  render(<Projects />);
-  render(<Contact />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/components/*.js"],
+  content: ["./src/components/*.jsx"],
   theme: {
     extend: {},
   },
